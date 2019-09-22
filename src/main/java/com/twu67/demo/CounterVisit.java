e9aecdf011a8121337888d33ba9eef882ee99e64
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2019-09-23 01:17:37 +0800

    [Xiaojun] Add metric for home page

---
+++ src/main/java/com/twu67/demo/CounterVisit.java
@@ -0,0 +1,16 @@
+package com.twu67.demo;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+public class CounterVisit {
+  private final Counter counter;
+
+  public CounterVisit(MeterRegistry registry) {
+    counter = Counter.builder("countHomePageVisitedTimes.total").description("the counter of home page").register(registry);
+  }
+
+  public void Increment() {
+    counter.increment();
+  }
+}
